0509b6884990ed00dc0b51a48bec71d4e6918c4b
Abhilash Kasula <58025056+abhilashkasula@users.noreply.github.com>
2020-05-21 22:45:28 +0530

    taking port from env

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
 module.exports = {
-  REDIS_URL: process.env.REDIS_URL || 'redis://localhost:6379'
+  REDIS_URL: process.env.REDIS_URL || 'redis://localhost:6379',
+  PORT: process.env.PORT || 8000
 }
